5e3c2ad4a268c673402ae73d7081d458db661cfe
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2024-11-19 21:31:06 +0500

    second commit

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {data} from './../../data'
 import Image from 'next/image'
+import Comment from '@/component/Comment'
 const page = async ({params}:{params:Promise<{id:number}>}) => {
    const id = (await params).id
 
@@ -9,13 +10,31 @@ const filterBlog = data.findIndex((item)=>item.id==id)
 const {image , title , shortDescription , fullContent } = data[filterBlog]
 
   return (
-    <div className='w-full h-auto bg-slate-200 flex justify-center items-center flex-col p-20 gap-y-10'>
-s      <h1 className='text-4xl font-bold'>{title}</h1>
+    <div className='w-full h-auto bg-slate-200 pb-20 '>
+    <div className=' flex justify-center items-center flex-col p-20 gap-y-10'>
+  <h1 className='text-4xl font-bold xs:text-center lg:text-left'>{title}</h1>
       <Image src={image} alt="image" width={500} height={500}/>
-      <h2 className='text-3xl lg:w-[500px] leading-10'>{shortDescription}</h2>
-      <p className='lg:w-[500px] text-xl leading-10'>{fullContent}</p>
+      <h2 className='lg:text-3xl lg:w-[500px] leading-10 font-medium'>{shortDescription}</h2>
+      <p className='lg:w-[500px] lg:text-xl leading-10 text-justify'>{fullContent}</p>
+    </div>
+    <div>
+      <Comment/>
+    </div>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default page
---
+++ src/component/Comment.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React, {  useEffect, useState } from "react";
+
+const Comment = () => {
+  const [comments, setComments] = useState<string[]>([]);
+
+  const [newComment, setNewComment] = useState<string>("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setComments((prevComment) => [...prevComment, newComment]);
+  };
+
+  useEffect(() => {
+    const savedComment = JSON.parse(localStorage.getItem('comments')!)||[];
+    setNewComment(savedComment);
+  }, []);
+
+  useEffect(() => {
+    const setComments = JSON.stringify(
+      localStorage.setItem("comments", JSON.stringify(newComment))
+    );
+  }, [comments]);
+
+  return (
+    <div className=" justify-center items-center flex flex-col space-y-10">
+      <div>
+        <h1 className="text-3xl font-relway font-bold">Comments</h1>
+        {comments.map((com, index) => (
+          <p key={index} className="border-b  border-[3px] ">{com}</p>
+        ))}
+      </div>
+      <h1 className="text-2xl font-relway font-bold text-center">
+        Give your feedback here!
+      </h1>
+
+      <form onSubmit={handleSubmit} className="flex flex-col space-y-10">
+        <textarea
+          cols={20}
+          rows={3}
+          name="commentSection"
+          placeholder="Enter Your comment ....."
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          className="px-4 py-5 outline-none border-black border-[2px]"
+        ></textarea>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Comment;
