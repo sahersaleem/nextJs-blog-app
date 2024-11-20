eff9d9984a56fba418d370aff7be4f5acfb15ba7
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2024-11-20 08:12:47 +0500

    fourth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import WhyChooseSection from "@/component/WhyChooseSection";
 export default function Home() {
   return (
     <div className=" relative h-screen w-full bg-[url(/images/bg2.png)] bg-cover bg-center ">
-      <div className="inset-0 absolute bg-black opacity-60 shadow-xl"></div>
+      <div className="inset-0 absolute bg-black opacity-10 shadow-xl"></div>
         <div className="z-40 relative">
           <Navbar />
           <Hero />
--- src/component/Comment.tsx
+++ src/component/Comment.tsx
@@ -1,27 +1,47 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import React, {  ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 
+type CommentType = {
+  username: string;
+  comment: string;
+};
 const Comment = () => {
-  const [comments, setComments] = useState<string[]>([]);
-
+  const [comments, setComments] = useState<CommentType[]>([]);
+  const [username, setUsername] = useState<string>("");
   const [newComment, setNewComment] = useState<string>("");
 
-  const handleSubmit = (e:any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-    setComments((prevComment) => [...prevComment, newComment]);
+
+    setComments((prevComment) => 
+       [
+        
+           ...prevComment,{
+
+          username: username,
+          comment: newComment,
+           
+        },
+      ]
+    );
+
+
+    setUsername("")
+    setNewComment("")
   };
 
   useEffect(() => {
-    const savedComment = JSON.parse(localStorage.getItem('comments')!)||[];
-    setNewComment(savedComment);
-  },[]);
+    const savedComment = JSON.parse(localStorage.getItem("comments")!||"[]")||[];
+
+    setComments(savedComment)
+  }, []);
 
   useEffect(() => {
-    const setComments = JSON.stringify(
-      localStorage.setItem("comments", JSON.stringify(newComment))
-    );
+    
+      localStorage.setItem("comments",JSON.stringify (comments))
+  
   }, [comments]);
 
   return (
@@ -29,7 +49,10 @@ const Comment = () => {
       <div>
         <h1 className="text-3xl font-relway font-bold">Comments</h1>
         {comments.map((com, index) => (
-          <p key={index} className="border-b  border-[3px] ">{com}</p>
+          <div key={index} className="border-b border-black mb-5">
+            <p className="font-bold">{com.username}</p>
+            <p>{com.comment}</p>
+          </div>
         ))}
       </div>
       <h1 className="text-2xl font-relway font-bold text-center">
@@ -37,12 +60,21 @@ const Comment = () => {
       </h1>
 
       <form onSubmit={handleSubmit} className="flex flex-col space-y-10">
+        <input
+          type="string"
+          name="name"
+          value={username||""}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          placeholder="Enter your name"
+        />
         <textarea
           cols={20}
           rows={3}
           name="commentSection"
           placeholder="Enter Your comment ....."
-          value={newComment}
+          value={newComment||""}
           onChange={(e) => setNewComment(e.target.value)}
           className="px-4 py-5 outline-none border-black border-[2px]"
         ></textarea>
